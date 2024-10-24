22b1ad2ce3f3f81ad6114f43276747016e0d1814
bollshoy <b.orlov7709@gmail.com>
2024-10-24 14:18:02 +0300

    upd test

---
+++ Data/TestData.js
@@ -0,0 +1,13 @@
+import test1 from '../src/assets/test1.png';
+import test2 from '../src/assets/test2.png';
+import test3 from '../src/assets/test3.png';
+import test4 from '../src/assets/test4.png';
+
+export const TestData = [
+	{id: 1, src: test1, alt: test1, title: 'Nvidia Geforce RTX 3070 + Core I5 12400f'},
+	{id: 2, src: test2, alt: test2, title: 'Nvidia Geforce GTX 1660S + Core I3 12100f'},
+	{id: 3, src: test3, alt: test3, title: 'Nvidia Geforce RTX 2060 + Core I5 12600K'},
+	{id: 4, src: test4, alt: test4, title: 'Nvidia Geforce RTX 3060 + Ryzen 5 5600'},
+];
+
+export default TestData;
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import googleDrive from '../assets/googleDrive.svg';
-import {NavLink} from "react-router-dom";
+import { TestData } from "../../Data/TestData.js";
 import './_Test.scss';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Test = () => {
+	const sectionRef = useRef(null);
+
+	useEffect(() => {
+		const section = sectionRef.current;
+
+		gsap.fromTo(section.querySelectorAll('.test__item'),
+				{ opacity: 0, y: 50 },
+				{
+					opacity: 1,
+					y: 0,
+					duration: 1,
+					stagger: 0.2,
+					ease: 'power2.out',
+					scrollTrigger: {
+						trigger: section,
+						start: 'top 80%', // Начало анимации, когда элемент на 80% в видимой области
+						once: true, // Анимация проигрывается один раз
+					},
+				}
+		);
+	}, []);
+
 	return (
-			<section className="test">
+			<section className="test" ref={sectionRef}>
 				<div className="test__container container">
 					<div className="test__text">
 						<h4 className="test__title">
@@ -14,14 +40,12 @@ const Test = () => {
 						<p className="test__text">В игре Counter-Strike 2</p>
 					</div>
 					<div className="test__content">
-						<button className="test__btn">
-							<img src={googleDrive} alt="googleDrive" className="test__btn-img"/>
-							<NavLink className={'test__link'}
-							         to={'https://docs.google.com/spreadsheets/d/152QqeeHofqn8fXe0O76v8l33R3QqDkhr/edit?gid=830117446#gid=830117446'}
-							         target={'_blank'}>
-								Открыть таблицу тестов
-							</NavLink>
-						</button>
+						{TestData.map((item, index) => (
+								<div className="test__item" key={index}>
+									<span className="test__item-title">{item.title}</span>
+									<img src={item.src} alt={item.alt} className="test__img"/>
+								</div>
+						))}
 					</div>
 				</div>
 			</section>
